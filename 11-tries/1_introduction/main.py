c10f8c3eb334e5e415b463dc41cadc3f0ca09c6e
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2026-06-21 22:03:46 +0700

    TRIES: introduction to tries

---
+++ 11-tries/1_introduction/main.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+
+class Trie:
+    def add(self, word: str) -> None:
+        current_level = self.root
+        for char in word:
+            if char not in current_level:
+                current_level[char] = {}
+            current_level = current_level[char]
+        current_level[self.end_symbol] = True
+
+    # don't touch below this line
+
+    def __init__(self) -> None:
+        self.root = {}
+        self.end_symbol = "*"
